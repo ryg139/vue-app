70f66aae51f347d0572f4ad1283fb59fd5aba858
renyuguang319 <ryg18335844165@126.com>
2020-01-07 16:14:06 +0800

    顾客端APP

---
+++ src/views/manager/address/List.vue
@@ -0,0 +1,46 @@
+<template>
+    <briup-fulllayout title="常用地址">
+        <!-- {{address}} -->
+        <van-list>
+        <van-cell
+            v-for="item in addresses"
+            :key="item.id"
+            :title="item.province+' '+item.city+' '+item.area+' '+item.address"/>
+        </van-list>
+        <br>
+        <van-button block type="default" @click="toAddressEditHandler">添加</van-button>
+    </briup-fulllayout>
+</template>
+
+<script>
+import {get} from '../../../http/axios'
+import {mapState} from 'vuex'
+export default {
+    data(){
+        return{
+            addresses:[]
+        }
+    },
+    computed:{
+        ...mapState("user",["info"])
+    },
+    created(){
+        this.loadAddress()
+    },
+    methods:{
+       loadAddress(){
+            let id=this.info.id;
+            let url="/address/findByCustomerId?id="+id;
+            get(url).then((response)=>{
+                this.addresses=response.data;
+            })
+       },
+       toAddressEditHandler(){
+           this.$router.push("/manager/address_edit");
+       }
+    }
+}
+</script>
+<style scoped>
+
+</style>
